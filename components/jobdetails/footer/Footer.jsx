c18d6d0539acd941b447b677e2cd976bc728d422
RomanLakhnov <lakhnov.roman@gmail.com>
2023-10-02 17:39:28 +0300

    created

---
+++ components/jobdetails/footer/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Image, Linking, Text, TouchableOpacity, View } from 'react-native'
+
+import { icons } from '../../../constants'
+import styles from './footer.style'
+const Footer = ({ url }) => {
+	return (
+		<View style={styles.container}>
+			<TouchableOpacity style={styles.likeBtn}>
+				<Image
+					source={icons.heartOutline}
+					resizeMode='contain'
+					style={styles.likeBtnImage}
+				/>
+			</TouchableOpacity>
+			<TouchableOpacity
+				style={styles.applyBtn}
+				onPress={() => Linking.openURL(url)}
+			>
+				<Text style={styles.applyBtnText}>Apply fo job</Text>
+			</TouchableOpacity>
+		</View>
+	)
+}
+
+export default Footer
